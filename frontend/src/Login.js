6764f1ecbec2a6dd35cd3d68f7d958c9976df0cd
gthmbl <gouthambala68@gmail.com>
2025-03-27 18:56:36 +0000

    Fix hardcoded :5000 API URL

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -49,7 +49,7 @@ const Login = ({ onLoginSuccess }) => {
     }
     setIsLoading(true);
     try {
-      const response = await fetch("http://18.130.231.134:5000/api/register", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
